51d3bb44f7dd3bf583ccc756928884258bf5b1a7
John_Yang <jiahong9yang@126.com>
2019-03-21 16:50:59 +0800

    生产者消费者demo

--- src/main/java/jhclass/ProducerSample.java
+++ src/main/java/jhclass/ProducerSample.java
@@ -1,5 +1,9 @@
 package jhclass;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,5 +17,15 @@ public class ProducerSample {
         props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
         props.put("zk.connect","127.0.0.1:2181");
+
+        String topic = "test-topic";
+
+        Producer<String,String> producer = new KafkaProducer<String, String>(props);
+
+        producer.send(new ProducerRecord<String, String>(topic,"idea-key2","java-message 1"));
+        producer.send(new ProducerRecord<String, String>(topic,"idea-key2","java-message 2"));
+        producer.send(new ProducerRecord<String, String>(topic,"idea-key2","java-message 3"));
+
+        producer.close();
     }
 }
